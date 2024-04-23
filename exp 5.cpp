7bcd9d2521032db1b92f005b721949e2b22ebadb
satyam42004 <157016027+satyam42004@users.noreply.github.com>
2024-04-23 17:58:44 +0530

    Add files via upload

--- exp 5.cpp
+++ exp 5.cpp
@@ -1,1 +1,68 @@
-
+/*
+Construct an expression tree from the given prefix expression eg. +--a*bc/def and
+traverse it using post order traversal (non recursive) and then delete the entire tree.
+*/
+//Ganesh bodakhe
+//A21
+#include<iostream>
+#include<stack>
+using namespace std;
+char exprsn[] = {'+', '-', '-', 'a', '*', 'b', 'c', '/', 'd', 'e', 'f'};
+struct node {
+struct node* left;
+struct node* right;
+char data;
+};
+stack<node*> stk;
+void createExpressionTree() {
+struct node* ptr1;
+struct node* ptr2;
+for (int i = 10; i > -1; i--) {
+struct node* nn = new node;
+nn->left = NULL;
+nn->right = NULL;
+if (exprsn[i] != '*' && exprsn[i] != '+' && exprsn[i] != '-' && exprsn[i] != '/') {
+nn->data = exprsn[i];
+stk.push(nn);
+}
+else {
+ptr1 = stk.top();
+stk.pop();
+ptr2 = stk.top();
+stk.pop();
+nn->data = exprsn[i];
+nn->left = ptr1;
+nn->right = ptr2;
+stk.push(nn);
+}
+}
+}
+void postOrderTraversal(node* root) {
+stack<node*> s1;
+stack<char> s2;
+s1.push(root);
+while (!s1.empty()) {
+struct node* current = s1.top();
+s1.pop();
+s2.push(current->data);
+if (current->left) {
+s1.push(current->left);
+}
+if (current->right) {
+s1.push(current->right);
+}
+}
+while (!s2.empty()) {
+cout << s2.top() << " ";
+s2.pop();
+}
+}
+int main() {
+createExpressionTree();
+cout<<"Expression tree is created !!";
+cout<<"\nPost Order Traversal is : [";
+struct node* root = stk.top();
+postOrderTraversal(root);
+cout<<"]";
+return 0;
+}
--- exp 7(b).cpp
+++ exp 7(b).cpp
@@ -1,1 +1,75 @@
-
+/*
+There are flight paths between cities. If there is a flight between city A and city B then
+there is an edge between the cities. The cost of the edge can be the time that flight take to
+reach city B from A, or the amount of fuel used for the journey. Represent this as a graph.
+The node can be represented by airport name or name of the city. Use adjacency list
+representation of the graph or use adjacency matrix representation of the graph. Check
+whether the graph is connected or not. Justify the storage representation used.
+*/
+//Ganesh bodakhe
+//A 21
+#include<iostream>
+using namespace std;
+string city[10][10];
+void create()
+{
+for(int i=0;i<10;i++){
+for(int j=0;j<10;j++)
+{
+city[i][j]="----";
+}
+}
+}
+void enterpath()
+{
+int scode,dcode;
+cout<<"\nEnter City code for Source city :";
+cin>>scode;
+cout<<"\nEnter City code for Destination city :";
+cin>>dcode;
+for(int i=0;i<10;i++){
+for(int j=0;j<10;j++)
+{
+if((i==scode && j==dcode)){
+cout<<"\nEnter the Time/Distance(Enter in 4 digits like 1-->0001) :";
+cin>>city[scode][dcode];
+}
+}
+}
+}
+void displaymatrix()
+{ int ind=0;
+for(int i=0;i<10;i++)
+{
+cout<<endl;
+cout<<"["<<ind<<")";
+ind++;
+for(int j=0;j<10;j++)
+{
+cout<<" "<<city[i][j]<<" ";
+}
+cout<<"]";
+}
+}
+int main()
+{
+int ch;
+cout<<"\n*********************| Cities In India |***************************";
+cout<<"\nCode City Name\n0) PUNE\n1) MUMBAI\n2) NAGPUR\n3) SANGAMANER\n4) SHIKRAPUR\n5) NASHIK\n6) USMANABAD\n7) DHULE\n8) A.NAGAR\n9) SAMBHAJINAGAR";
+cout<<"\n*******************************************************************";
+create();
+while(ch!=3)
+{
+cout<<"\n\n1)Create\n2)Display Matrix\n3)Exit(x)\nEnter your choice :";
+cin>>ch;
+switch(ch)
+{
+case 1:
+enterpath();
+break;
+case 2:
+displaymatrix();
+break;
+}
+}
+}
